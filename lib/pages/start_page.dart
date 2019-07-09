a1a1c0af9422ce4633abd45dfbc2e3f1fb4c6552
CoMatu <matusevichvv@gmail.com>
2019-07-09 12:50:31 +0500

    начал делать логин 2

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,4 +1,8 @@
+import 'package:carstat/pages/dashboard_page.dart';
+import 'package:carstat/pages/login_page.dart';
+import 'package:carstat/services/auth_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:carstat/services/auth_service.dart';
 
 class StartPage extends StatefulWidget {
   @override
@@ -15,15 +19,49 @@ class _StartPageState extends State<StartPage> {
   AuthStatus authStatus = AuthStatus.notDetermined;
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final BaseAuth auth = AuthProvider.of(context).auth;
+    auth.currentUser().then((String userId) {
+      setState(() {
+        authStatus = userId == null ? AuthStatus.notSignedIn : AuthStatus.signedIn;
+      });
+    });
+  }
+
+  void _signedIn() {
+    setState(() {
+      authStatus = AuthStatus.signedIn;
+    });
+  }
+
+  void _signedOut() {
+    setState(() {
+      authStatus = AuthStatus.notSignedIn;
+    });
+  }
+  @override
+  @override
   Widget build(BuildContext context) {
+    switch (authStatus) {
+      case AuthStatus.notDetermined:
+        return _buildWaitingScreen();
+      case AuthStatus.notSignedIn:
+        return LoginPage(
+//          onSignedIn: _signedIn,
+        );
+      case AuthStatus.signedIn:
+        return DashboardPage(
+//          onSignedOut: _signedOut,
+        );
+    }
+    return null;
+  }
+  Widget _buildWaitingScreen() {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.yellow),
-          )
-        ],
+      body: Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
       ),
     );
   }
