7f2abe3b2276f6c464cfeb372aad511550487bea
CoMatu <matusevichvv@gmail.com>
2019-07-09 16:31:57 +0500

    logout from dashboard_page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,17 +1,32 @@
 import 'package:carstat/pages/start_page.dart';
+import 'package:carstat/services/auth_provider.dart';
+import 'package:carstat/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'dashboard_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatefulWidget {
-  MainPage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MainPage({Key key}) : super(key: key);
 
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  _MainPageState({this.onSignedOut});
+  final VoidCallback onSignedOut;
+
+
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final BaseAuth auth = AuthProvider.of(context).auth;
+      await auth.signOut();
+      onSignedOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,13 +35,14 @@ class _MainPageState extends State<MainPage> {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              //TODO add logout button to menu
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                  color: Colors.yellow[600]
-              ),
-            )
+            UserAccountsDrawerHeader(
+            accountName: null,
+            accountEmail: null),
+            ListTile(
+              title: Text('Выход из аккаунта'),
+              trailing: Icon(FontAwesomeIcons.signOutAlt),
+              onTap: () => _signOut(context),
+            ),
           ],
         ),
       ),
