eb3a713d73a6094133fa2a3ab3fdec15389509b4
CoMatu <matusevichvv@gmail.com>
2019-08-06 09:55:46 +0500

    сделана карточка 2

--- lib/components/car_card.dart
+++ lib/components/car_card.dart
@@ -17,44 +17,64 @@ class CarCard extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               children: <Widget>[
                 Expanded(child: Text(car.carName)),
+                Text('редактировать', style: TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black38
+                ),),
                 IconButton(
                   icon: Icon(Icons.delete),
                   onPressed: () {},
-                )
+                ),
               ],
             ),
-            subtitle: Wrap(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text(car.carMark),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text(car.carModel),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text(car.carYear.toString() + ' г.'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Text('Пробег: ' + car.carMileage.toString() + ' км.'),
-                      GestureDetector(
-                        child: Icon(Icons.edit),
-                        onTap: () {},
-                      ),
-                    ],
+            subtitle: Container(
+              padding: EdgeInsets.only(bottom: 6.0),
+              child: Wrap(
+                alignment: WrapAlignment.spaceBetween,
+                children: <Widget>[
+                  Divider(),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Text(car.carMark),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Text(car.carModel),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Text(car.carYear.toString() + ' г.'),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 8.0),
-                  child: Text('VIN: ' + car.carVin),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text('Пробег: ' + car.carMileage.toString() + ' км.'),
+                        Container(
+                          padding: EdgeInsets.only(left: 8.0),
+                          child: GestureDetector(
+                            child: Icon(Icons.edit),
+                            onTap: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text('VIN: ' + car.carVin),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
