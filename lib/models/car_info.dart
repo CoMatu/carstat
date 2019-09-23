e9a292ec7ba23ab7b82ab20862a58a152fd86a41
CoMatu <matusevichvv@gmail.com>
2019-09-23 16:29:31 +0500

    вывод статистики в работе 22

---
+++ lib/models/car_info.dart
@@ -0,0 +1,16 @@
+import 'package:carstat/models/entry.dart';
+import 'package:carstat/models/operation.dart';
+
+import 'car.dart';
+
+class CarInfo {
+  final Car car;
+  final List<EntryInfo> entryInfoList;
+  CarInfo(this.car, this.entryInfoList);
+}
+
+class EntryInfo {
+  final Entry entry;
+  final List<Operation> entryOperations;
+  EntryInfo(this.entry, this.entryOperations);
+}
